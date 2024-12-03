7bdc3f4baec110f2f3300301df235b8a181ac423
manavos <155997183+manavos@users.noreply.github.com>
2024-12-03 00:16:04 -0800

    Update httpd.c

--- httpd.c
+++ httpd.c
@@ -10,195 +10,199 @@
 #include <fcntl.h>
 
 
+void create_response(int client_fd, const char *method, const char *path) {
+    // create correct path
+    char correct_path[512];
+    snprintf(correct_path, sizeof(correct_path), ".%s", path);
 
-#define FIFO_IN "server_in"
-#define FIFO_OUT "server_out"
-
-void create_response(FILE *network, const char *method, const char *path){
-
-    //only look in current directory, add . to beginning of bath
-   char correct_path[512];
-   snprintf(correct_path, sizeof(correct_path), ".%s", path);
-   FILE *file = fopen(correct_path, "r"); //or malloc??
-
-
-   if (file == NULL){
-      fprintf(network, "HTTP/1.0 404 Not Found\r\n\r\n");
-      return;
-
-   }
-
-   // Get file size
-    struct stat file_size;
-    if (stat(correct_path, &file_size) == -1) {
-        perror("stat");
+    FILE *file = fopen(correct_path, "r");
+    if (file == NULL) {
+        dprintf(client_fd, "HTTP/1.0 404 Not Found\r\n");
+        dprintf(client_fd, "Content-Type: text/plain\r\n");
+        dprintf(client_fd, "\r\n");
+        dprintf(client_fd, "404 Not Found\n");
         return;
     }
-    
 
-    // Send HTTP response header <-- HEAD (only sends info about file)
-    fprintf(network, "HTTP/1.0 200 OK\r\n");
-    fprintf(network, "Content-Type: text/html\r\n");
-    fprintf(network, "Content-Length: %lld\r\n", (long long) file_size.st_size);
-    fprintf(network, "\r\n");
-    fflush(network);
-    
-   
-    //printf("HTTP Response:\nHTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: %lld\r\n\r\n", (long long) file_size.st_size);
+    struct stat file_stat;
+    if (stat(correct_path, &file_stat) == -1) {
+        perror("stat");
+        fclose(file);
+        return;
+    }
 
-    
-    if (strcmp(method, "GET") == 0) { //GET sends header AND file content
-        // Send file content
-        char buffer[1024];
-        size_t bytes;
-        while ((bytes = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-            //printf("content: %s", buffer);
-            if (fwrite(buffer, 1, bytes, network) < bytes) { //writing file content
-                perror("Client disconnected or write error");
-                break; // Exit the loop if write fails
-            }
+    //send headers
+    dprintf(client_fd, "HTTP/1.0 200 OK\r\n");
+    dprintf(client_fd, "Content-Type: text/html\r\n");
+    dprintf(client_fd, "Content-Length: %lld\r\n", (long long)file_stat.st_size);
+    dprintf(client_fd, "\r\n");
 
+    // If method is GET, send the file content
+    if (strcmp(method, "GET") == 0) {
+        char response[1024];
+        size_t bytes_read;
+        while ((bytes_read = fread(response, 1, sizeof(response), file)) > 0) {
+            write(client_fd, response, bytes_read);
         }
     }
 
     fclose(file);
-
 }
 
+void cgi(int client_fd, const char *program, const char *query) {
+    printf("entered cgi");
+    char correct_path[256];
+    snprintf(correct_path, sizeof(correct_path), "./cgi-like/%s", program);
 
-void handle_request(int nfd)
-{
-    
-   FILE *network = fdopen(nfd, "r");
-   char *line = NULL;
-   size_t size;
-   //ssize_t num;
-
-   if (network == NULL)
-   {
-      perror("fdopen");
-      close(nfd);
-      return;
-   }
-
-  if(getline(&line, &size, network) > 0){
-    char type[16], filename[256], version[16];
-    //printf("Request line: %s\n", line);
-    if (sscanf(line, "%s %s %s", type, filename, version) == 3) {
-        if (strcmp(filename, "/favicon.ico") == 0) { //send
-                fprintf(network, "HTTP/1.0 404 Not Found\r\n");
-                fprintf(network, "Content-Type: text/plain\r\n");
-                fprintf(network, "Connection: close\r\n");
-                fprintf(network, "\r\n");
-                fflush(network);
-
-        }
-        else if (strcmp(type, "GET") == 0 || strcmp(type, "HEAD") == 0){
-            create_response(network, type, filename);
-        }
-        else {
-            fprintf(network, "HTTP/1.0 501 Not Implemented\r\n\r\n"); //not get or head
-        }
-    } else { //not enough info
-        fprintf(network, "HTTP/1.0 400 Bad Request\r\n\r\n");
-    }
-    //printf("Method: %s, Path: %s, Version: %s\n", type, filename, version);
-
-  }
-  
-
-   free(line);
-   fclose(network);
-}
-
-
-void cgi(FILE *network, const char *program, const char *query){
-   //until ./cgi-like is found
-   char sanitized_program[256];
-   snprintf(sanitized_program, sizeof(sanitized_program), "./cgi-like/%s", program);
-   
-   //error message
-   if (access(sanitized_program, X_OK) != 0) {
-        fprintf(network, "HTTP/1.0 404 Not Found\r\n\r\n");
+    //check if program exists
+    if (access(correct_path, X_OK) != 0) {
+        dprintf(client_fd, "HTTP/1.0 404 Not Found\r\n\r\n");
         return;
     }
 
+    //temporary files for output
     char temp_file[64];
     snprintf(temp_file, sizeof(temp_file), "/tmp/cgi_output_%d", getpid());
     FILE *out = fopen(temp_file, "w+");
-    if (!out) {
-        fprintf(network, "HTTP/1.0 500 Internal Server Error\r\n\r\n");
+    if (out == NULL) {
+        dprintf(client_fd, "HTTP/1.0 500 Internal Server Error\r\n\r\n");
         return;
     }
+    fclose(out);
 
     pid_t pid = fork();
-    if (pid == 0) { // Child process
-        dup2(fileno(out), STDOUT_FILENO);
+    if (pid == -1) { //error
+        dprintf(client_fd, "HTTP/1.0 500 Internal Server Error\r\n\r\n");
+        return;
+
+    } else if (pid == 0) { // child
+        //redirecting output of program to temp file
+        out = fopen(temp_file, "w"); //opening temp file
+        if (!out) {
+            perror("fopen");
+            exit(1);
+        }
+        dup2(fileno(out), STDOUT_FILENO); //redirecting
         fclose(out);
 
-        // Parse query into arguments
-        char *args[256] = { sanitized_program };
+        // Prepare arguments for exec
+        char *args[256] = { correct_path };
         int i = 1;
-        char *arg = strtok(query, "&");
-        while (arg && i < 255) {
-            args[i++] = arg;
-            arg = strtok(NULL, "&");
+        if (query) {
+            char *arg = strtok(query, "&"); //seperating path by &
+            while (arg && i < 255) {
+                args[i++] = arg; //adding to array
+                arg = strtok(NULL, "&"); //moving to next
+            }
         }
         args[i] = NULL;
 
         execvp(args[0], args);
-        perror("execvp"); // Only reached on error
+        perror("execvp"); //execvp error
         exit(1);
     }
 
-    fclose(out);
-    waitpid(pid, NULL, 0);
 
-    // Read and send CGI output
-        struct stat file_size;
-    if (stat(temp_file, &file_size) == -1) {
-        perror("stat");
+    waitpid(pid, NULL, 0); //parent
+
+    //gather output
+    struct stat file_stat;
+    if (stat(temp_file, &file_stat) == -1) {
+        dprintf(client_fd, "HTTP/1.0 500 Internal Server Error\r\n\r\n");
+        remove(temp_file);
         return;
     }
 
-    fprintf(network, "HTTP/1.0 200 OK\r\n");
-    fprintf(network, "Content-Type: text/html\r\n");
-    fprintf(network, "Content-Length: %lld\r\n", file_size.st_size);
-    fprintf(network, "\r\n");
-
-    char buffer[1024];
-    size_t bytes;
-    while ((bytes = fread(buffer, 1, sizeof(buffer), out)) > 0) {
-        fwrite(buffer, 1, bytes, network);
+    FILE *temp = fopen(temp_file, "r");
+    if (!temp) {
+        dprintf(client_fd, "HTTP/1.0 500 Internal Server Error\r\n\r\n");
+        remove(temp_file);
+        return;
     }
 
-    fclose(out);
-    remove(temp_file);
+    //send headers
+    dprintf(client_fd, "HTTP/1.0 200 OK\r\n");
+    dprintf(client_fd, "Content-Type: text/plain\r\n");
+    dprintf(client_fd, "Content-Length: %lld\r\n", (long long)file_stat.st_size);
+    dprintf(client_fd, "\r\n");
 
+    //send content
+    char response[1024];
+    size_t bytes_read;
+    while ((bytes_read = fread(response, 1, sizeof(response), temp)) > 0) {
+        write(client_fd, response, bytes_read);
+    }
+
+    fclose(temp);
+    remove(temp_file);
+}
+
+
+
+void handle_request(int nfd) {
+    FILE *network = fdopen(nfd, "r+");
+    if (network == NULL) {
+        perror("fdopen");
+        close(nfd);
+        return;
+    }
+
+    char line[1024];
+    while (fgets(line, sizeof(line), network)) {
+        char method[16], path[256], version[16];
+        line[strcspn(line, "\r\n")] = '\0';
+
+        if (sscanf(line, "%s %s %s", method, path, version) != 3) {
+            fprintf(network, "HTTP/1.0 400 Bad Request\r\n\r\n");
+            printf("here");
+            fflush(network);
+            break;
+        }
+        
+
+        if (strcmp(method, "GET") == 0 || strcmp(method, "HEAD") == 0) {
+            if (strncmp(path, "/cgi-like/", 10) == 0) {
+                    char *program = strtok(path + 10, "?");
+                    char *query = strtok(NULL, "?");
+                    cgi(fileno(network), program, query);
+                }
+
+            else {
+                create_response(fileno(network), method, path);
+            }
+
+
+            
+        } else {
+            // unsupported HTTP method
+            fprintf(network, "HTTP/1.0 501 Not Implemented\r\n\r\n");
+            fflush(network);
+        }
+
+        // stop processing if connection is closed
+        if (feof(network) || ferror(network)) {
+            break;
+        }
+    }
+
+    fclose(network);
 }
 
 
 
 
 
-void run_service(int fd)
-{
-   while (1)
-   {
-      int nfd = accept_connection(fd);
-      if (nfd != -1)
-      {
-       
+void run_service(int fd) {
+    while (1) {
+        int nfd = accept_connection(fd);
+        if (nfd != -1) {
             printf("Connection established\n");
             handle_request(nfd);
             close(nfd);
             printf("Connection closed\n");
-            //exit(0);
-         }
-        
-      }
-   }
-
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -217,3 +221,6 @@ int main(int argc, char *argv[])
 
    return 0;
 }
+
+
+
