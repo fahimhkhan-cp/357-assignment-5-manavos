5007a9960460f9d231e4174c1df5a2640210ee46
manavos <155997183+manavos@users.noreply.github.com>
2024-12-03 20:46:55 -0800

    final updates httpd.c

--- httpd.c
+++ httpd.c
@@ -11,19 +11,17 @@
 
 
 void create_response(int client_fd, const char *method, const char *path) {
-    // create correct path
+    //simplify path
     char correct_path[512];
     snprintf(correct_path, sizeof(correct_path), ".%s", path);
 
     FILE *file = fopen(correct_path, "r");
-    if (file == NULL) {
-        dprintf(client_fd, "HTTP/1.0 404 Not Found\r\n");
-        dprintf(client_fd, "Content-Type: text/plain\r\n");
-        dprintf(client_fd, "\r\n");
-        dprintf(client_fd, "404 Not Found\n");
+    if (file == NULL) { //file not found
+        dprintf(client_fd, "404 Not Found\r\n\r\n");
         return;
     }
 
+    //get file size
     struct stat file_stat;
     if (stat(correct_path, &file_stat) == -1) {
         perror("stat");
@@ -49,84 +47,75 @@ void create_response(int client_fd, const char *method, const char *path) {
     fclose(file);
 }
 
-void cgi(int client_fd, const char *program, const char *query) {
-    printf("entered cgi");
-    char correct_path[256];
-    snprintf(correct_path, sizeof(correct_path), "./cgi-like/%s", program);
+void cgi(int client_fd, const char *program, const char *query){
+     if (strstr(program, "..") != NULL) {
+        dprintf(client_fd, "HTTP/1.0 400 Bad Request\r\n\r\n");
+        return;
+    }
 
-    //check if program exists
-    if (access(correct_path, X_OK) != 0) {
+   //simplify path
+   char correct_path[256];
+   snprintf(correct_path, sizeof(correct_path), "./cgi-like/%s", program);
+   
+   
+   //check if file is executable
+   if (access(correct_path, X_OK) != 0) {
         dprintf(client_fd, "HTTP/1.0 404 Not Found\r\n\r\n");
         return;
     }
 
-    //temporary files for output
+    //make new path for temp file
     char temp_file[64];
     snprintf(temp_file, sizeof(temp_file), "/tmp/cgi_output_%d", getpid());
-    FILE *out = fopen(temp_file, "w+");
-    if (out == NULL) {
-        dprintf(client_fd, "HTTP/1.0 500 Internal Server Error\r\n\r\n");
+
+    FILE *temp = fopen(temp_file, "w+"); //w to write to file
+    if (temp == NULL) {
+        dprintf(client_fd, "404 Not Found\r\n\r\n");
         return;
     }
-    fclose(out);
 
     pid_t pid = fork();
-    if (pid == -1) { //error
-        dprintf(client_fd, "HTTP/1.0 500 Internal Server Error\r\n\r\n");
-        return;
+    if (pid == 0) { // Child process
+        dup2(fileno(temp), STDOUT_FILENO); //write output to temp file
+        dup2(fileno(temp), STDERR_FILENO); //write errors (if any) to temp file
 
-    } else if (pid == 0) { // child
-        //redirecting output of program to temp file
-        out = fopen(temp_file, "w"); //opening temp file
-        if (!out) {
-            perror("fopen");
-            exit(1);
-        }
-        dup2(fileno(out), STDOUT_FILENO); //redirecting
-        fclose(out);
-
-        // Prepare arguments for exec
-        char *args[256] = { correct_path };
+        //search for arguments
+        char *args[256] = {correct_path};
         int i = 1;
-        if (query) {
-            char *arg = strtok(query, "&"); //seperating path by &
-            while (arg && i < 255) {
-                args[i++] = arg; //adding to array
-                arg = strtok(NULL, "&"); //moving to next
-            }
+        char *arg = strtok(query, "&"); 
+        while (arg && i < 255) {
+            args[i++] = arg;
+            arg = strtok(NULL, "&");
         }
         args[i] = NULL;
-
-        execvp(args[0], args);
-        perror("execvp"); //execvp error
+        
+        execvp(args[0], args); //execute program with arguments
+        perror("execvp"); //error
         exit(1);
     }
 
-
     waitpid(pid, NULL, 0); //parent
 
-    //gather output
-    struct stat file_stat;
-    if (stat(temp_file, &file_stat) == -1) {
-        dprintf(client_fd, "HTTP/1.0 500 Internal Server Error\r\n\r\n");
-        remove(temp_file);
-        return;
-    }
-
-    FILE *temp = fopen(temp_file, "r");
-    if (!temp) {
-        dprintf(client_fd, "HTTP/1.0 500 Internal Server Error\r\n\r\n");
-        remove(temp_file);
+    fclose(temp);
+    temp = fopen(temp_file, "r"); //open temp in read mode
+    if (temp == NULL) {
+        dprintf(client_fd, "404 Not Found\r\n\r\n");
         return;
     }
 
     //send headers
+    struct stat file_size;
+    if (stat(temp_file, &file_size) == -1) {
+        perror("stat");
+        return;
+    }
+
     dprintf(client_fd, "HTTP/1.0 200 OK\r\n");
-    dprintf(client_fd, "Content-Type: text/plain\r\n");
-    dprintf(client_fd, "Content-Length: %lld\r\n", (long long)file_stat.st_size);
+    dprintf(client_fd, "Content-Type: text/html\r\n");
+    dprintf(client_fd, "Content-Length: %lld\r\n", file_size.st_size);
     dprintf(client_fd, "\r\n");
 
-    //send content
+    //send cgi output
     char response[1024];
     size_t bytes_read;
     while ((bytes_read = fread(response, 1, sizeof(response), temp)) > 0) {
@@ -135,73 +124,74 @@ void cgi(int client_fd, const char *program, const char *query) {
 
     fclose(temp);
     remove(temp_file);
+
 }
 
 
+void handle_request(int nfd){
+   FILE *network = fdopen(nfd, "r");
+   char *line = NULL;
+   size_t size;
+   ssize_t num;
 
-void handle_request(int nfd) {
-    FILE *network = fdopen(nfd, "r+");
-    if (network == NULL) {
-        perror("fdopen");
-        close(nfd);
-        return;
-    }
+   if (network == NULL)
+   {
+      perror("fdopen");
+      close(nfd);
+      return;
+   }
 
-    char line[1024];
-    while (fgets(line, sizeof(line), network)) {
-        char method[16], path[256], version[16];
-        line[strcspn(line, "\r\n")] = '\0';
+   if ((num = getline(&line, &size, network)) >= 0){
 
-        if (sscanf(line, "%s %s %s", method, path, version) != 3) {
-            fprintf(network, "HTTP/1.0 400 Bad Request\r\n\r\n");
-            printf("here");
-            fflush(network);
-            break;
-        }
-        
+    //parse request
+      char method[16], path[256], version[16];
+      if (sscanf(line, "%15s %255s %15s", method, path, version) == 3){
 
-        if (strcmp(method, "GET") == 0 || strcmp(method, "HEAD") == 0) {
-            if (strncmp(path, "/cgi-like/", 10) == 0) {
-                    char *program = strtok(path + 10, "?");
-                    char *query = strtok(NULL, "?");
-                    cgi(fileno(network), program, query);
+        if (strcmp(method, "GET") == 0 || strcmp(method, "HEAD") == 0) { //get or head requests
+
+            if (strncmp(path, "/cgi-like/", 10) == 0){ //cgi-like
+                char *program = strtok(path + 10, "?");
+                char *query = strtok(NULL, "?");
+                cgi(nfd, program, query);
                 }
 
             else {
-                create_response(fileno(network), method, path);
+                create_response(nfd, method, path); //regular request
+                }
             }
 
-
-            
-        } else {
-            // unsupported HTTP method
+          else {
             fprintf(network, "HTTP/1.0 501 Not Implemented\r\n\r\n");
-            fflush(network);
-        }
+            }
 
-        // stop processing if connection is closed
-        if (feof(network) || ferror(network)) {
-            break;
+          }
+
+        else { 
+            fprintf(network, "HTTP/1.0 400 Bad Request\r\n\r\n");
+            printf("here");
+            fflush(network);
+
         }
+        
     }
 
-    fclose(network);
+      
+   free(line);
+   fclose(network);
 }
 
-
-
-
-
-void run_service(int fd) {
-    while (1) {
-        int nfd = accept_connection(fd);
-        if (nfd != -1) {
-            printf("Connection established\n");
-            handle_request(nfd);
-            close(nfd);
-            printf("Connection closed\n");
-        }
-    }
+void run_service(int fd)
+{
+   while (1)
+   {
+      int nfd = accept_connection(fd);
+      if (nfd != -1)
+      {
+         printf("Connection established\n");
+         handle_request(nfd);
+         printf("Connection closed\n");
+      }
+   }
 }
 
 int main(int argc, char *argv[])
@@ -221,6 +211,3 @@ int main(int argc, char *argv[])
 
    return 0;
 }
-
-
-
